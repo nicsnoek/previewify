9b7e1b2b1403b2cea8143afd51f8db7cb36feff9
NicSnoek <nic@twipsy.local>
2011-02-24 13:04:26 +1100

    Small cleanups

--- lib/previewify.rb
+++ lib/previewify.rb
@@ -13,9 +13,9 @@ module Previewify
   class Options
 
     def initialize(options_hash, preview_table_name, preview_columns)
-      @options_hash       = options_hash
+      @options_hash = options_hash
       @preview_table_name = preview_table_name
-      @preview_columns    = preview_columns
+      @preview_columns = preview_columns
     end
 
     def published_version_table_name
@@ -92,7 +92,7 @@ module Previewify
         delegate :published_on, :to => :latest_published, :allow_nil => true
 
         def latest_published
-          primary_key_name  = self.class.previewify_options.primary_key_attribute_name
+          primary_key_name = self.class.previewify_options.primary_key_attribute_name
           primary_key_value = self.send(primary_key_name)
 
           @latest_published ||= self.class.published_version_class.latest_published_by_primary_key(primary_key_value)
@@ -114,12 +114,12 @@ module Previewify
           if show_preview? || self == published_version_class
             super(*args)
           else
-            published_version_class.latest_published.send(args[0], *args.slice(1..-1))
+            published_version_class.latest_published.send(*args)
           end
         end
 
         def publish!
-          latest_published         = take_down!
+          latest_published = take_down!
           latest_published_version = latest_published.try(:version) || 0
           self.class.published_version_class.publish(self, latest_published_version + 1)
         end
@@ -167,8 +167,8 @@ module Previewify
         end
         previewify_options.published_columns.each do |published_column|
           connection.add_column previewify_options.published_version_table_name, published_column.name, published_column.type,
-                                :limit     => published_column.limit,
-                                :scale     => published_column.scale,
+                                :limit => published_column.limit,
+                                :scale => published_column.scale,
                                 :precision => published_column.precision
         end
       end
@@ -220,11 +220,11 @@ module Previewify
 
           attributes_to_publish = previewify_options.published_attributes(preview.attributes)
           attributes_to_publish.merge!(
-              previewify_options.version_attribute_name        => version,
+              previewify_options.version_attribute_name => version,
               previewify_options.published_flag_attribute_name => true,
-              :published_on                                    => Time.now
+              :published_on => Time.now
           )
-          published_version    = self.new(attributes_to_publish)
+          published_version = self.new(attributes_to_publish)
           if preview.respond_to?(:id)
             published_version.id = preview.id # won't mass-assign
           end
