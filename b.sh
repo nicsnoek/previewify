f161875b1a5cedff0638a03ec1aa674ca0b757fd
Nic Snoek <nicsnoek@yahoo.com>
2011-11-22 12:40:39 +1100

    Install bundler gem in build script

--- b.sh
+++ b.sh
@@ -3,6 +3,7 @@
 rvm use "ruby-1.9.2-p290@previewify"
 # Do any setup
 # e.g. possibly do 'rake db:migrate db:test:prepare' here
+gem install bundler
 bundle install
 # Finally, run your tests
 rake test
